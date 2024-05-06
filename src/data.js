cce97c0fe19175001d8d7674a5aa6fe39cb04519
Adedoyin <akeshinr@asu.edu>
2024-05-06 15:14:13 -0700

    Add data.js to the repository

---
+++ src/data.js
@@ -0,0 +1,8 @@
+const companyInfo = {
+    address: '3021 S Valerie Dr, AZ 85286',
+    phone1: '302-669-5402',
+    phone2: '215-294-0792',
+    email: 'matreadcare@gmail.com'
+};
+
+export default companyInfo;
