c7705a50aa71e3cc002b2f53eff1a8575adfad16
Adedoyin <akeshinr@asu.edu>
2024-05-06 14:37:07 -0700

    Update .gitignore to ignore specific files in src

--- src/data.js
+++
@@ -1,8 +0,0 @@
-const companyInfo = {
-    address: '3021 S Valerie Dr, AZ 85286',
-    phone1: '302-669-5402',
-    phone2: '215-294-0792',
-    email: 'matreadcare@gmail.com'
-};
-
-export default companyInfo;
