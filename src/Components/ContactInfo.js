9c3151a55563400bcf10151dc6d615e3fd5178a6
Adedoyin <akeshinr@asu.edu>
2024-05-06 15:02:34 -0700

    Fixed some bugs, and tested all functionality

--- src/Components/ContactInfo.js
+++ src/Components/ContactInfo.js
@@ -1,7 +1,7 @@
 import '../cssFiles/ContactInfo.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouseUser} from '@fortawesome/free-solid-svg-icons';
-import companyInfo  from '../data';
+import companyInfo  from '../data.js';
 
 
 function ContactInfo()
